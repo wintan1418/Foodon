181daaa8382ae21619c40b8032960a30e5b58ea6
wintan1418 <wintan1418@gmail.com>
2024-06-13 08:42:25 +0100

    deploy

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Foodon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.css_compressor = nil
 
     # Configuration for the application, engines, and railties goes here.
     #
